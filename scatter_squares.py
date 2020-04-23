8111a8c11b7b25933264c5099d02b898e1a86196
otr0624 <otr0624@mail.com>
2020-04-23 11:36:34 -0400

    Added colormap formatting to data, and exported to .png file

--- scatter_squares.py
+++ scatter_squares.py
@@ -5,7 +5,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes
 ax.set_title("Square Numbers", fontsize=24)
@@ -18,4 +18,4 @@ ax.tick_params(axis='both', which='major', labelsize=14)
 # Set the range for each axis
 ax.axis([0, 1100, 0, 1100000])
 
-plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
